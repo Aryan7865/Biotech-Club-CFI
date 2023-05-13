f11c00273f6cc8a5b031ae13fa13b543384f43b1
Aryan Shirbhate <121824389+Aryan7865@users.noreply.github.com>
2023-05-14 00:40:46 +0530

    Update biotechclubQ3.py

--- biotechclubQ3.py
+++ biotechclubQ3.py
@@ -1,22 +1,22 @@
 #Aryan Shirbhate BS22B009
 # Codon table
 codontable = {
-    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
-    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
-    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
-    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
-    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
-    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
-    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
-    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
-    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
-    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
-    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
-    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
-    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
-    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-    'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
-    'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W'
+    "ATA": "I", "ATC": "I", "ATT": "I", "ATG": "M",
+    "ACA": "T", "ACC": "T", "ACG": "T", "ACT": "T",
+    "AAC": "N", "AAT": "N", "AAA": "K", "AAG": "K",
+    "AGC": "S", "AGT": "S", "AGA": "R", "AGG": "R",
+    "CTA": "L", "CTC": "L", "CTG": "L", "CTT": "L",
+    "CCA": "P", "CCC": "P", "CCG": "P", "CCT": "P",
+    "CAC": "H", "CAT": "H", "CAA": "Q", "CAG": "Q",
+    "CGA": "R", "CGC": "R", "CGG": "R", "CGT": "R",
+    "GTA": "V", "GTC": "V", "GTG": "V", "GTT": "V",
+    "GCA": "A", "GCC": "A", "GCG": "A", "GCT": "A",
+    "GAC": "D", "GAT": "D", "GAA": "E", "GAG": "E",
+    "GGA": "G", "GGC": "G", "GGG": "G", "GGT": "G",
+    "TCA": "S", "TCC": "S", "TCG": "S", "TCT": "S",
+    "TTC": "F", "TTT": "F", "TTA": "L", "TTG": "L",
+    "TAC": "Y", "TAT": "Y", "TAA": "*", "TAG": "*",
+    "TGC": "C", "TGT": "C", "TGA": "*", "TGG": "W"
 }
 
 def translate(dnaseq):
@@ -37,7 +37,7 @@ def translate(dnaseq):
     return proteinseq
 
 def reversecomplement(dnaseq):
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    complement = {"A": "T", "C": "G", "G": "C", "T": "A"}
     revcomp = ''.join([complement.get(base, 'N') for base in reversed(dnaseq)])
     return revcomp
 
